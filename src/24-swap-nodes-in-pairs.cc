89502222c0e4f8144b0ddbc197524946f1270cee
Xiangdong Zeng <xdzeng96@gmail.com>
2019-12-07 12:43:53 +0800

    leetcode 24

---
+++ src/24-swap-nodes-in-pairs.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "leetcode_util.h"
+using namespace std;
+using leetcode::ListNode;
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head != nullptr && head->next != nullptr) {
+            auto p0 = head, p2 = swapPairs(head->next->next);
+            head = p0->next;
+            head->next = p0;
+            head->next->next = p2;
+        }
+        return head;
+    }
+};
+
+int main() {
+    Solution sol;
+    auto head = new ListNode{1,2,3,4};
+    cout << leetcode::to_string(head) << endl;
+    head = sol.swapPairs(head);
+    cout << leetcode::to_string(head) << endl;
+}

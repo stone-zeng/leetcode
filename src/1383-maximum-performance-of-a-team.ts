a0a63cd2768008f24b6cadf932f6b86221cd948c
Xiangdong Zeng <xdzeng96@gmail.com>
2022-09-11 22:51:09 +0800

    leetcode 1383: optimize

--- src/1383-maximum-performance-of-a-team.ts
+++ src/1383-maximum-performance-of-a-team.ts
@@ -2,41 +2,47 @@
 // https://leetcode.com/problems/maximum-performance-of-a-team/
 
 function maxPerformance(n: number, speed: number[], efficiency: number[], k: number): number {
-    const modulo = 10 ** 9 + 7;
-
+    const modulo = BigInt(10 ** 9 + 7);
     const pairs = speed.map((s, i) => [s, efficiency[i]]).sort((p1, p2) => p1[1] - p2[1]);
-    let res = 0;
-
-    // pairs.forEach(([s, e], i) => {
-    //     const sum = sumLargest(
-    //         pairs.slice(i + 1).map((p) => p[0]),
-    //         Math.min(k - 1, n - i)
-    //     );
-    //     res = Math.max(res, (sum + s) * e);
-    // });
-
     const speedSorted = speed.sort((a, b) => b - a);
-    pairs.forEach(([s, e], i) => {
-        speedSorted.splice(speedSorted.indexOf(s), 1);
-        const sum = speedSorted.slice(0, Math.min(k - 1, n - i)).reduce((i, s) => i + s, 0);
-        res = Math.max(res, (sum + s) * e);
+    let sum = speedSorted.slice(0, k - 1).reduce((i, s) => i + s, 0);
+    let res = 0n;
+    pairs.forEach(([s, e]) => {
+        const pos = binarySearch(speedSorted, s);
+        speedSorted.splice(pos, 1);
+        if (pos < k - 1) {
+            sum = sum - s + (k - 1 > speedSorted.length ? 0 : speedSorted[k - 2]);
+        }
+        res = max(res, BigInt(sum + s) * BigInt(e));
     });
-
     return Number(res % modulo);
 }
 
-// const sumLargest = (nums: number[], n: number) => {
-//     if (!n) return 0;
-//     return nums
-//         .sort((a, b) => b - a)
-//         .slice(0, n)
-//         .reduce((i, s) => i + s, 0);
-// };
+const binarySearch = (nums: number[], n: number) => {
+    let begin = 0;
+    let end = nums.length;
+    while (begin + 1 <= end) {
+        const mid = Math.floor((begin + end) / 2);
+        const numMid = nums[mid];
+        if (numMid === n) return mid;
+        if (numMid < n) {
+            end = mid;
+        } else {
+            begin = mid + 1;
+        }
+    }
+    return -1;
+};
+
+const max = <T>(a: T, b: T) => (a > b ? a : b);
+
+import data from '../data.json';
 
 console.log(
     [
         { n: 6, speed: [2, 10, 3, 1, 5, 8], efficiency: [5, 4, 3, 9, 7, 2], k: 2 },
         { n: 6, speed: [2, 10, 3, 1, 5, 8], efficiency: [5, 4, 3, 9, 7, 2], k: 3 },
         { n: 6, speed: [2, 10, 3, 1, 5, 8], efficiency: [5, 4, 3, 9, 7, 2], k: 4 },
+        data,
     ].map(({ n, speed, efficiency, k }) => maxPerformance(n, speed, efficiency, k))
 );
